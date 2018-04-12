b6c19354feeaab711933d172c939f925cb9b1f04
Jacob Somera <jsome635@hawaii.edu>
2018-04-12 10:37:41 -1000

    Revert "Merge branch 'issue-11'"
    This reverts commit 380e6d0c8b54bc9748f5603dc8e0779de58d782b, reversing
    changes made to c72e8f6194b678577e6dc0b2b648402c6212a238.

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -22,11 +22,25 @@ class Footer extends React.Component {
         <footer style={footerStyle}>
           <div style={divStyle}>
             <br />
+            <Grid verticalAlign='middle' textAlign='center'>
+              <Grid.Column width={5}>
+                <a href="https://look-n-cook.github.io/">Documentation</a>
+              </Grid.Column>
+              <Grid.Column width={5}>
+                <a href="https://github.com/look-n-cook/Look-n-Cook">
+                  <Icon name='github'/>
+                  GitHub
+                </a>
+              </Grid.Column>
+            </Grid>
             <div className="ui center aligned container">
+              <br />
+              <br />
               Department of Information and Computer Sciences <br />
               University of Hawaii<br />
               Honolulu, HI 96822<br />
               <br />
+<<<<<<< HEAD
               <Grid verticalAlign='middle' textAlign='center'>
                 <Grid.Column width={2}>
                   <a style={linkStyle} href="https://look-n-cook.github.io/">
@@ -42,6 +56,8 @@ class Footer extends React.Component {
                 </Grid.Column>
               </Grid>
               <br />
+=======
+>>>>>>> parent of 2ffb0c9... Added Icon to documentation link and change position.
             </div>
           </div>
         </footer>
