e58fb35fdaba7dfc1bcbdde6f0a84f4dff5061e2
Jacob Somera <jsome635@hawaii.edu>
2018-04-11 20:11:38 -1000

    Rounded separators.

--- app/imports/ui/components/Separator.jsx
+++ app/imports/ui/components/Separator.jsx
@@ -3,16 +3,17 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Separator extends React.Component {
   render() {
-    const divStyle = {
+    const separatorStyle = {
       paddingTop: '5px',
       background: '#376507',
       margin: '25px',
       marginLeft: '5%',
       marginRight: '5%',
+      borderRadius: '2.5px',
     };
     return (
         <footer>
-          <div style={divStyle}>
+          <div style={separatorStyle}>
           </div>
         </footer>
     );
