846b253a009cfb4485401d6fe75d7f06ec85e702
Charles Rafael <cdrafael@hawaii.edu>
2018-04-24 12:02:31 -1000

    created schema for vendors

---
+++ app/imports/api/vendor/vendor.js
@@ -0,0 +1,35 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Create a Meteor collection. */
+const Vendors = new Mongo.Collection('Vendors');
+
+/** Create a schema to constrain the structure of documents associated with this collection. */
+const VendorSchema = new SimpleSchema({
+  name: String,
+  description: String,
+  ingredients: {
+    type: Array,
+  },
+  'ingredients.$': {
+    type: Object,
+  },
+  'ingredients.$.name': {
+    type: String,
+  },
+  'ingredients.$.size': {
+    type: String,
+  },
+  'ingredients.$.price': {
+    type: Number,
+  },
+  owner: String,
+  createdAt: Date,
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Vendors.attachSchema(VendorSchema);
+
+/** Make the collection and schema available to other code. */
+export { Vendors, VendorSchema };
--- app/imports/startup/both/index.js
+++ app/imports/startup/both/index.js
@@ -1,1 +1,2 @@
 import '/imports/api/recipe';
+import '/imports/api/vendor';
