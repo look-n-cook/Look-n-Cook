ab11ca6b321381ee73a6bb3d5e65caf6ea2e5e72
g-tolentino <gmtolent@hawaii.edu>
2018-05-01 23:51:59 -1000

    Fixed Profile Cards

--- app/imports/ui/components/Recipes.jsx
+++ app/imports/ui/components/Recipes.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Card, Image, Button, Feed } from 'semantic-ui-react';
+import { Card, Image, Button, Feed, Grid, Menu } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { Recipes } from '/imports/api/recipe/recipe';
-import { withRouter, NavLink } from 'react-router-dom';
+import { withRouter, NavLink, Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Recipe extends React.Component {
@@ -36,31 +36,57 @@ class Recipe extends React.Component {
     const cardStyle = {
       background: '#F3E2C4',
     };
+    const dietArray = [];
+    if (this.props.recipe.vegan === true) { dietArray.push('Vegan'); }
+    if (this.props.recipe.glutenFree === true) { dietArray.push('Gluten Free'); }
+    if (this.props.recipe.dairyFree === true) { dietArray.push('Dairy Free'); }
     return (
-      <Card centered style={cardStyle}>
-        <Card.Content>
-          <Card.Header>
-            {this.props.recipe.name}
-          </Card.Header>
-          <div style={ { marginTop: '10px' } }>
-            <Image floated='center' height='350px' width='300px' src={this.props.recipe.image} />
-          </div>
+        <Card centered style={cardStyle} link>
+          <Card.Content>
+            <Card.Header>
+              {this.props.recipe.name}
+            </Card.Header>
+            <div style={{ marginTop: '10px' }}>
+              <Link to={`/recipe/${this.props.recipe._id}`}>
+                <Image floated='center' height='350px' width='300px' src={this.props.recipe.image}/>
+              </Link>
+            </div>
+
             <Card.Description>
-              <div style={ { marginTop: '375px', marginBottom: '20px' } }>
-              {this.props.recipe.description}
+              <div style={{ marginTop: '375px', marginBottom: '10px' }}>
+                {this.props.recipe.description}
               </div>
             </Card.Description>
-          <Card.Meta>
-            Added on
-          </Card.Meta>
-          <Feed.Date content={this.props.recipe.createdAt.toLocaleDateString('en-US')} />
-        </Card.Content>
-        <Card.Content extra>
-          <Button basic onClick={this.onClick}>Delete</Button>
-          <Button basic as={NavLink} exact to="/recipe"> Recipe </Button>
-          <Button basic as={NavLink} exact to={`/edit/${this.props.recipe._id}`}> Edit </Button>
-        </Card.Content>
-      </Card>
+
+            <Grid columns={2} textAlign={'center'}>
+              <Grid.Column>
+                <Card.Meta>
+                  Created By
+                </Card.Meta>
+                <Feed.Content content={this.props.recipe.owner}/>
+              </Grid.Column>
+              <Grid.Column>
+                <Card.Meta>
+                  Added on
+                </Card.Meta>
+                <Feed.Date content={this.props.recipe.createdAt.toLocaleDateString('en-US')}/>
+              </Grid.Column>
+            </Grid>
+          </Card.Content>
+          <Card.Content extra>
+            <Grid textAlign={'center'}>
+              <Menu compact borderless text textAlign={'center'}>
+                {dietArray.map((diet, index) => <Menu.Item key={index}>{`${diet}`}</Menu.Item>)}
+              </Menu>
+            </Grid>
+          </Card.Content>
+          <Card.Content extra>
+            <Grid textAlign={'center'} columns={2}>
+            <Grid.Column><Button fluid basic onClick={this.onClick}>Delete</Button></Grid.Column>
+            <Grid.Column><Button fluid basic as={NavLink} exact to={`/edit/${this.props.recipe._id}`}>Edit</Button></Grid.Column>
+            </Grid>
+          </Card.Content>
+        </Card>
     );
   }
 }
