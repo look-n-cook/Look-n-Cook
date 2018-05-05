95336e17ccf51fef1a8dcb56ba590b4aa0fddb6e
Jacob Somera <jsome635@hawaii.edu>
2018-05-04 15:51:07 -1000

    Finished issue-34

--- app/imports/ui/components/VendorItemAdmin.jsx
+++ app/imports/ui/components/VendorItemAdmin.jsx
@@ -3,21 +3,26 @@ import { Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 /** Renders a single row in the List Stuff (Admin) table. See pages/ListStuffAdmin.jsx. */
-class StuffItemAdmin extends React.Component {
+class VendorItemAdmin extends React.Component {
   render() {
+    const ingredientOwners = _.map(this.props.ingredients, (ingredient) => ingredient.owner);
+    console.log(ingredientOwners);
     return (
         <Table.Row>
-          <Table.Cell></Table.Cell>
-          <Table.Cell></Table.Cell>
-          <Table.Cell></Table.Cell>
+          <Table.Cell>{this.props.vendor.emails['0'].address}</Table.Cell>
+          <Table.Cell>{
+            (_.filter(ingredientOwners, (owner) => owner === this.props.vendor.emails['0'].address)).length
+          }</Table.Cell>
+          <Table.Cell>{this.props.vendor.createdAt.toLocaleDateString('en-US')}</Table.Cell>
         </Table.Row>
     );
   }
 }
 
 /** Require a document to be passed to this component. */
-StuffItemAdmin.propTypes = {
-  stuff: PropTypes.object.isRequired,
+VendorItemAdmin.propTypes = {
+  vendor: PropTypes.object.isRequired,
+  ingredients: PropTypes.array.isRequired,
 };
 
-export default StuffItemAdmin;
+export default VendorItemAdmin;
--- app/imports/ui/pages/ListVendorsAdmin.jsx
+++ app/imports/ui/pages/ListVendorsAdmin.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Container, Table, Header, Loader } from 'semantic-ui-react';
-import { Stuffs } from '/imports/api/stuff/stuff';
+import { Vendors } from '/imports/api/vendor/vendor';
+import { Accounts } from '/imports/api/accounts/accounts';
 import VendorItemAdmin from '/imports/ui/components/VendorItemAdmin';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-class ListVendorsAdmin extends React.Component {
+class ListIngredientsAdmin extends React.Component {
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
-    return (this.props.ready) ? this.renderPage() : <Loader>Getting data</Loader>;
+    return (this.props.readyAccounts && this.props.readyIngredients) ?
+        this.renderPage() : <Loader>Getting data</Loader>;
   }
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+    const vendors = _.filter(
+        (this.props.accounts),
+        (account) => _.contains((account.roles), 'vendor'),
+    );
+
+    console.log(vendors);
+
     return (
         <Container>
-          <Header as="h2" textAlign="center">List Vendors</Header>
+          <Header as="h2" textAlign="center">List Users</Header>
           <Table celled>
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Location</Table.HeaderCell>
+                <Table.HeaderCell>Vendor</Table.HeaderCell>
                 <Table.HeaderCell># Products</Table.HeaderCell>
+                <Table.HeaderCell>Joined On</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
             <Table.Body>
-              {this.props.stuffs.map((stuff) => <VendorItemAdmin key={stuff._id} stuff={stuff} />)}
+              {vendors.map((vendor) => <VendorItemAdmin
+                  key={vendor._id} vendor={vendor} ingredients={this.props.ingredients}/>)
+              }
             </Table.Body>
           </Table>
         </Container>
@@ -37,17 +48,22 @@ class ListVendorsAdmin extends React.Component {
 }
 
 /** Require an array of Stuff documents in the props. */
-ListVendorsAdmin.propTypes = {
-  stuffs: PropTypes.array.isRequired,
-  ready: PropTypes.bool.isRequired,
+ListIngredientsAdmin.propTypes = {
+  accounts: PropTypes.array.isRequired,
+  ingredients: PropTypes.array.isRequired,
+  readyIngredients: PropTypes.bool.isRequired,
+  readyAccounts: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Stuff');
+  const subscriptionIngredients = Meteor.subscribe('VendorList');
+  const subscriptionAccounts = Meteor.subscribe('AccountsAdmin');
   return {
-    stuffs: Stuffs.find({}).fetch(),
-    ready: subscription.ready(),
+    accounts: Accounts.find({}).fetch(),
+    ingredients: Vendors.find({}).fetch(),
+    readyIngredients: subscriptionIngredients.ready(),
+    readyAccounts: subscriptionAccounts.ready(),
   };
-})(ListVendorsAdmin);
+})(ListIngredientsAdmin);
